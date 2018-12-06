44a44b048f25202f3bc9b500efbbf1b3c62f24d0
Lucas Marques <lucasmarques430@gmail.com>
2018-12-06 12:29:30 -0200

    pronto

--- lingprog/subscriptions/views.py
+++ lingprog/subscriptions/views.py
@@ -1,24 +1,9 @@
-from django.core import mail
-from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
-from django.template.loader import render_to_string
 
 from lingprog.subscriptions.forms import SubscriptionForm
 
 def subscribe(request):
-    if request.method == 'POST':
-        form = SubscriptionForm(request.POST)
-        if form.is_valid():
-            body = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
-            mail.send_mail('Confirmacao de inscricao',
-                           body,
-                           'contato@lingprog.com.br',
-                           ['contato@lingprog.com.br', form.cleaned_data['email']])
-            return HttpResponseRedirect('/inscricao/')
-        else:
-            return render(request, 'subscriptions/subscription_form.html')
-    else:
-        context = {'form': SubscriptionForm()}
-        return render(request, 'subscriptions/subscription_form.html', context)
+  context = {'form': SubscriptionForm()}
+  return render(request, 'subscriptions/subscription_form.html', context)
 
 
--- lingprog/wsgi.py
+++ lingprog/wsgi.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lingprog.settings')
 
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
+
+application = get_wsgi_application()
+application = WhiteNoise(application)
